a48449f105253f959be67849430005a772e75f74
Mike <mikeglopez.91@gmail.com>
2020-05-07 16:03:08 -0500

    Swap scrollview for flatlist

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import {
   View,
   TextInput,
   Button,
-  ScrollView
+  ScrollView,
+  FlatList
 } from 'react-native';
 
 export default function App() {
@@ -17,7 +18,10 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals((currentGoals) => [...courseGoals, enteredGoal]);
+    setCourseGoals((currentGoals) => [
+      ...courseGoals,
+      { uid: Math.random().toString(), value: enteredGoal }
+    ]);
   };
 
   return (
@@ -31,13 +35,15 @@ export default function App() {
         />
         <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
+      <FlatList
+        keyExtractor={(item, index) => item.uid}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )}
+      />
     </View>
   );
 }
