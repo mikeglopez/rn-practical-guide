1d869a5c3e844b6516b52467f7ed38c6ff781506
Mike <mikeglopez.91@gmail.com>
2020-05-08 11:07:21 -0500

    Add cancel button and styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,6 +8,11 @@ const GoalInput = (props) => {
     setEnteredGoal(enteredText);
   };
 
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal('');
+  };
+
   return (
     <Modal visible={props.visible} animationType='slide'>
       <View style={styles.inputContainer}>
@@ -17,7 +22,14 @@ const GoalInput = (props) => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title='ADD' onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title='CANCEL' color='#FF0000' onPress={props.onCancel} />
+          </View>
+          <View style={styles.button}>
+            <Button title='ADD' onPress={addGoalHandler} />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -25,15 +37,24 @@ const GoalInput = (props) => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1, // Lets container take as much space as parent container
+    justifyContent: 'center',
     alignItems: 'center'
   },
   input: {
     width: '80%',
     borderColor: '#000000',
     borderWidth: 1,
-    padding: 10
+    padding: 10,
+    marginBottom: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%'
+  },
+  button: {
+    width: '40%'
   }
 });
 
